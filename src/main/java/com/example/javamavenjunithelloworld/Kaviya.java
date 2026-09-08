11b871ae8e031e828f242f41ebaa8a83b90698f9
kaviya-sharon14 <sharonkaviya70@gmail.com>
2026-09-08 16:02:54 +0530

    KAVIYAS0001 - Created HelloWorld Program

---
+++ src/main/java/com/example/javamavenjunithelloworld/Kaviya.java
@@ -0,0 +1,6 @@
+package com.example.javamavenjunithelloworld; 
+public class Kaviya { public static void main(String[] args) { 
+System.out.println("Hello World"); 
+System.out.println("I am Kaviya"); 
+} 
+} 
